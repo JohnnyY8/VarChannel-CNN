6f0f895e893cbd99a0e3b0b0ea628be82bc9fc8e
JohnnyY8 <johnny9141@hotmail.com>
2018-03-13 10:20:17 +0800

    Add run.sh.

---
+++ run.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+nohup python -u main.py \
+    --gpuId 0 \
+    --conv1KWidth 20 \
+    --conv1SWidth 5 \
+    --num4OutputChannels 20 \
+    --path4SaveModel ./files/intermediate/trainedModel0 \
+    --path4Summaries ./files/intermediate/summaries0 \
+    --path4SaveEggFiles ./files/intermediate/eggFiles0 \
+    1>./files/intermediate/info0.txt 2>&1 &
+
+nohup python -u main.py \
+    --gpuId 1 \
+    --conv1KWidth 30 \
+    --conv1SWidth 10 \
+    --num4OutputChannels 30 \
+    --path4SaveModel ./files/intermediate/trainedModel0 \
+    --path4Summaries ./files/intermediate/summaries0 \
+    --path4SaveEggFiles ./files/intermediate/eggFiles0 \
+    1>./files/intermediate/info0.txt 2>&1 &
