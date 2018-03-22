a05f676a7f738c0fa62b19b95902d03c848895e3
JohnnyY8 <johnny9141@hotmail.com>
2018-03-22 11:57:48 +0800

    Modify some hyper-parameters.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 nohup python -u main.py \
     --gpuId 0 \
-    --conv1KWidth 4 \
+    --conv1KWidth 16 \
     --conv1SWidth 1 \
     --num4OutputChannels 2 \
     --path4SaveModel ./files/intermediate/trainedModel0 \
@@ -12,10 +12,30 @@ nohup python -u main.py \
 
 nohup python -u main.py \
     --gpuId 1 \
-    --conv1KWidth 5 \
+    --conv1KWidth 17 \
     --conv1SWidth 1 \
     --num4OutputChannels 2 \
     --path4SaveModel ./files/intermediate/trainedModel1 \
     --path4Summaries ./files/intermediate/summaries \
     --path4SaveEggsFile ./files/intermediate/eggFiles1 \
     1>./files/intermediate/info1.txt 2>&1 &
+
+nohup python -u main.py \
+    --gpuId 2 \
+    --conv1KWidth 18 \
+    --conv1SWidth 1 \
+    --num4OutputChannels 2 \
+    --path4SaveModel ./files/intermediate/trainedModel2 \
+    --path4Summaries ./files/intermediate/summaries \
+    --path4SaveEggsFile ./files/intermediate/eggFiles2 \
+    1>./files/intermediate/info2.txt 2>&1 &
+
+nohup python -u main.py \
+    --gpuId 3 \
+    --conv1KWidth 19 \
+    --conv1SWidth 1 \
+    --num4OutputChannels 2 \
+    --path4SaveModel ./files/intermediate/trainedModel3 \
+    --path4Summaries ./files/intermediate/summaries \
+    --path4SaveEggsFile ./files/intermediate/eggFiles3 \
+    1>./files/intermediate/info3.txt 2>&1 &
