c1f963992355b73b106d25ac9fac219ae06756a8
JohnnyY8 <johnny9141@hotmail.com>
2018-03-23 10:11:52 +0800

    Modify some hyper-parameters.

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 nohup python -u main.py \
     --gpuId 0 \
-    --conv1KWidth 24 \
-    --conv1SWidth 1 \
+    --conv1KWidth 4 \
+    --conv1SWidth 2 \
     --num4OutputChannels 2 \
     --path4SaveModel ./files/intermediate/trainedModel0 \
     --path4Summaries ./files/intermediate/summaries \
@@ -12,30 +12,30 @@ nohup python -u main.py \
 
 nohup python -u main.py \
     --gpuId 1 \
-    --conv1KWidth 25 \
-    --conv1SWidth 1 \
+    --conv1KWidth 4 \
+    --conv1SWidth 3 \
     --num4OutputChannels 2 \
     --path4SaveModel ./files/intermediate/trainedModel1 \
     --path4Summaries ./files/intermediate/summaries \
     --path4SaveEggsFile ./files/intermediate/eggFiles1 \
     1>./files/intermediate/info1.txt 2>&1 &
 
-#nohup python -u main.py \
-#    --gpuId 2 \
-#    --conv1KWidth 22 \
-#    --conv1SWidth 1 \
-#    --num4OutputChannels 2 \
-#    --path4SaveModel ./files/intermediate/trainedModel2 \
-#    --path4Summaries ./files/intermediate/summaries \
-#    --path4SaveEggsFile ./files/intermediate/eggFiles2 \
-#    1>./files/intermediate/info2.txt 2>&1 &
-#
-#nohup python -u main.py \
-#    --gpuId 3 \
-#    --conv1KWidth 23 \
-#    --conv1SWidth 1 \
-#    --num4OutputChannels 2 \
-#    --path4SaveModel ./files/intermediate/trainedModel3 \
-#    --path4Summaries ./files/intermediate/summaries \
-#    --path4SaveEggsFile ./files/intermediate/eggFiles3 \
-#    1>./files/intermediate/info3.txt 2>&1 &
+nohup python -u main.py \
+    --gpuId 2 \
+    --conv1KWidth 4 \
+    --conv1SWidth 4 \
+    --num4OutputChannels 2 \
+    --path4SaveModel ./files/intermediate/trainedModel2 \
+    --path4Summaries ./files/intermediate/summaries \
+    --path4SaveEggsFile ./files/intermediate/eggFiles2 \
+    1>./files/intermediate/info2.txt 2>&1 &
+
+nohup python -u main.py \
+    --gpuId 3 \
+    --conv1KWidth 4 \
+    --conv1SWidth 5 \
+    --num4OutputChannels 2 \
+    --path4SaveModel ./files/intermediate/trainedModel3 \
+    --path4Summaries ./files/intermediate/summaries \
+    --path4SaveEggsFile ./files/intermediate/eggFiles3 \
+    1>./files/intermediate/info3.txt 2>&1 &
