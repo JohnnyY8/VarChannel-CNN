bcd0c5363c001d0bcaa54eb8d06242f4425f9dbe
JohnnyY8 <johnny9141@hotmail.com>
2018-04-18 09:19:10 +0800

    Modify some hyper-parameters.

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ nohup python -u main.py \
     --num4OutputChannels 2 \
     --num4FirstFC 200 \
     --num4SecondFC 10 \
-    --nWeight 13 \
+    --nWeight 17 \
     --path4SaveModel ./files/intermediate/trainedModel0 \
     --path4Summaries ./files/intermediate/summaries \
     --path4SaveEggsFile ./files/intermediate/eggFiles0 \
@@ -20,7 +20,7 @@ nohup python -u main.py \
     --num4OutputChannels 2 \
     --num4FirstFC 200 \
     --num4SecondFC 10 \
-    --nWeight 14 \
+    --nWeight 18 \
     --path4SaveModel ./files/intermediate/trainedModel1 \
     --path4Summaries ./files/intermediate/summaries \
     --path4SaveEggsFile ./files/intermediate/eggFiles1 \
@@ -33,7 +33,7 @@ nohup python -u main.py \
     --num4OutputChannels 2 \
     --num4FirstFC 200 \
     --num4SecondFC 10 \
-    --nWeight 15 \
+    --nWeight 19 \
     --path4SaveModel ./files/intermediate/trainedModel2 \
     --path4Summaries ./files/intermediate/summaries \
     --path4SaveEggsFile ./files/intermediate/eggFiles2 \
@@ -46,7 +46,7 @@ nohup python -u main.py \
     --num4OutputChannels 2 \
     --num4FirstFC 200 \
     --num4SecondFC 10 \
-    --nWeight 16 \
+    --nWeight 20 \
     --path4SaveModel ./files/intermediate/trainedModel3 \
     --path4Summaries ./files/intermediate/summaries \
     --path4SaveEggsFile ./files/intermediate/eggFiles3 \
